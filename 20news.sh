0415d16bf4360b3231f40ab0336adf8b87146cd5
Stefan Bunk <stefan.bunk@googlemail.com>
2016-09-26 14:02:22 +0200

    Update 20news.sh

--- 20news.sh
+++ 20news.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-python topicExp.py -s 20news train
-python topicExp.py -p 20news-train-11314-sep281-em100-last.topic.vec 20news train,test
-python classEval.py 20news topicprop
-python classEval.py 20news topic-wvavg
+#python topicExp.py -s 20news train
+
+
+#python topicExp.py -p 20news-train-11314-sep281-em100-last.topic.vec 20news train,test
+python topicExp.py \
+--corpus 20news \
+--vocabulary /data/wikipedia/2016-06-21/topic-models/topic.20news.50-1500.sentences/model.dim-200.skip-gram.embedding.restricted.vocab.counts \
+--embeddings /data/wikipedia/2016-06-21/topic-models/topic.20news.50-1500.sentences/dim-200.skip-gram.embedding.model.restricted.vocab.embedding \
+--set-names train,test \
+--max-iterations 100 \
+--proportions 20news-train-11314-sep281-em200-best.topic.vec
+#python classEval.py 20news topicprop
+
+#python classEval.py 20news topic-wvavg
